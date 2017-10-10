5c51055112d7c1da08b372c99ca40d0192f92c37
Tim Schmidt <tim.schmidt@ewe.net>
2017-10-10 04:04:36 +0200

    finished set 3, skipped last sub-challenge

--- s3c24.py
+++ s3c24.py
@@ -1,18 +1,34 @@
 from w4rumutils import *
 
 def randomCrypt(plain, key):
-    r = MT19937(key & 0xff)
+    r = MT19937(key & 0xffff)
     crypt = bytearray()
     
     for b in plain:
-        keystream = r.extract_number() & 0xf
+        keystream = r.extract_number() & 0xff
         crypt.append(b ^ keystream)
 
     return bytes(crypt)
 
-seed = 0x47455420544f20544845204348415041274149
-plain = b'chelo dis neis taxt'
-crypt = randomCrypt(plain, seed)
-print(plain)
-print(crypt)
-print(randomCrypt(crypt, seed))
+plain = b'A'*14
+pre = b''.join([bytes([randint(0,255)]) for x in range(randint(1, 100))])
+plain = pre + plain
+secretKey = randint(0,2**16)
+crypt = randomCrypt(plain, secretKey)
+c = 0
+for i in range(2**16):
+    # only print every 100 steps
+    if c % 100 == 0:
+        print("Trying seed " + hex(i))
+        c = 0
+    c += 1
+    testCrypt = randomCrypt(plain, i)
+    if crypt == testCrypt:
+        print("Secret key was " + hex(i))
+        print("(Confirmation: " + hex(secretKey) + ")")
+        print("Plain:")
+        print(plain)
+        print("Encrypted:")
+        print(crypt)
+        break
+
